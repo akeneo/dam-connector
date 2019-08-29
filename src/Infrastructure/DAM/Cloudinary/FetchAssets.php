bc95bfc95100c4f7968b7ade8fd84517f8b294ee
Willy <willy.mesnage@akeneo.com>
2019-08-29 14:14:10 +0200

    Fecth cloudinary assets

---
+++ src/Infrastructure/DAM/Cloudinary/FetchAssets.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AkeneoDAMConnector\Infrastructure\DAM\Cloudinary;
+
+use AkeneoDAMConnector\Application\DamAdapter\FetchAssets as FetchAssetsInterface;
+use AkeneoDAMConnector\Domain\Asset\DamAsset;
+use AkeneoDAMConnector\Domain\Asset\DamAssetCollection;
+use AkeneoDAMConnector\Domain\AssetFamily;
+
+class FetchAssets implements FetchAssetsInterface
+{
+    /** @var Search */
+    private $search;
+
+    public function __construct(Search $search)
+    {
+        $this->search = $search;
+    }
+
+    public function fetch(\DateTime $lastFetchDate, AssetFamily $assetFamily): DamAssetCollection
+    {
+        $expression = sprintf('tags:akeneo AND folder="%s"', $assetFamily->getCode());
+        $response = $this->search->search($expression, ['tags', 'context']);
+
+        $staticAttributes = ['resource_type', 'filename', 'url', 'secure_url', 'status', 'public_id'];
+        $damAssets = new DamAssetCollection();
+        $assets = $response['resources'] ?? [];
+        foreach ($assets as $asset) {
+            $damAsset = new DamAsset($assetFamily, 'en_US');
+
+            foreach ($staticAttributes as $staticAttribute) {
+                $damAsset->addValue($staticAttribute, $asset[$staticAttribute]);
+            }
+
+            foreach ($asset['context'] as $property => $value) {
+                $damAsset->addValue($property, $value);
+            }
+            $damAsset->addValue('tags', implode(', ', $asset['tags']));
+
+            $damAssets->addAsset($damAsset);
+        }
+
+        return $damAssets;
+    }
+}
