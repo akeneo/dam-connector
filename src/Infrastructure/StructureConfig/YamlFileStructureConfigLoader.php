a7b4aa232d53c0f6b1352f5c9ed2f2268f9c0c88
romain <romain@akeneo.com>
2019-09-02 11:36:25 +0200

    Rename namespace for Structure config

--- src/Infrastructure/StructureConfig/YamlFileStructureConfigLoader.php
+++ src/Infrastructure/StructureConfig/YamlFileStructureConfigLoader.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace AkeneoDAMConnector\Infrastructure\StructureConfiguration;
+namespace AkeneoDAMConnector\Infrastructure\StructureConfig;
 
 use AkeneoDAMConnector\Application\StructureConfig\StructureConfigLoader;
 use Symfony\Component\Yaml\Parser;
