15e54278007e59aec7b92d882b8bb68dcbe81e69
Paul Chasle <paul.chasle@akeneo.com>
2019-09-24 11:49:28 +0200

    Setup Cloudinary

---
+++ src/Infrastructure/DAM/Cloudinary/FetchAssets.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AkeneoDAMConnector\Infrastructure\DAM\Cloudinary;
+
+use AkeneoDAMConnector\Application\DamAdapter\FetchAssets as FetchAssetsInterface;
+use AkeneoDAMConnector\Domain\Asset\DamAsset;
+use AkeneoDAMConnector\Domain\Asset\DamAssetCollection;
+use AkeneoDAMConnector\Domain\Asset\DamAssetIdentifier;
+use AkeneoDAMConnector\Domain\AssetFamilyCode;
+use AkeneoDAMConnector\Domain\Locale;
+use AkeneoDAMConnector\Domain\ResourceType;
+
+class FetchAssets implements FetchAssetsInterface
+{
+    /** @var Search */
+    private $search;
+
+    public function __construct(Search $search)
+    {
+        $this->search = $search;
+    }
+
+    public function fetch(\DateTime $lastFetchDate, AssetFamilyCode $assetFamilyCode): DamAssetCollection
+    {
+        $expression = sprintf('tags:akeneo AND folder="%s"', (string) $assetFamilyCode);
+        $response = $this->search->search($expression, ['tags', 'context']);
+
+        $staticAttributes = ['filename', 'url', 'secure_url', 'status', 'public_id'];
+        $damAssets = new DamAssetCollection();
+        $assets = $response['resources'] ?? [];
+        foreach ($assets as $asset) {
+            $damAsset = new DamAsset(
+                new DamAssetIdentifier($asset['filename']),
+                $assetFamilyCode,
+                new Locale('en_US'),
+                new ResourceType($asset['resource_type'])
+            );
+
+            foreach ($staticAttributes as $staticAttribute) {
+                $damAsset->addValue($staticAttribute, $asset[$staticAttribute]);
+            }
+
+            foreach ($asset['context'] as $property => $value) {
+                $damAsset->addValue($property, $value);
+            }
+            $damAsset->addValue('tags', implode(', ', $asset['tags']));
+
+            $damAssets->addAsset($damAsset);
+        }
+
+        return $damAssets;
+    }
+}
