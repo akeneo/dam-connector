100a18ac0e1ae78771c2f80d4855d0c7c420d235
Willy <willy.mesnage@akeneo.com>
2019-09-25 10:55:34 +0200

    API-693: Attribute builder spec

---
+++ tests/Specification/Application/Mapping/AssetAttributeBuilderSpec.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types=1);
+
+namespace Specification\AkeneoDAMConnector\Application\Mapping;
+
+use AkeneoDAMConnector\Application\ConfigLoader;
+use AkeneoDAMConnector\Application\Mapping\AssetAttributeBuilder;
+use AkeneoDAMConnector\Domain\AssetAttributeCode;
+use AkeneoDAMConnector\Domain\AssetFamilyCode;
+use PhpSpec\ObjectBehavior;
+
+class AssetAttributeBuilderSpec extends ObjectBehavior
+{
+    public function let(ConfigLoader $structureConfigLoader): void
+    {
+        $this->beConstructedWith($structureConfigLoader);
+    }
+
+    function it_is_initializable(): void
+    {
+        $this->shouldHaveType(AssetAttributeBuilder::class);
+    }
+
+    function it_builds_an_attribute(
+        $structureConfigLoader,
+        AssetFamilyCode $familyCode,
+        AssetAttributeCode $attributeCode
+    ): void {
+        $structureConfigLoader->load()->willReturn($this->getConfig());
+
+        $familyCode->__toString()->willReturn('packshot');
+        $attributeCode->__toString()->willReturn('locale');
+
+        $attribute = $this->build($familyCode, $attributeCode);
+        $attribute->getCode()->shouldReturn($attributeCode);
+        $attribute->getType()->shouldReturn('text');
+        $attribute->isLocalizable()->shouldReturn(false);
+    }
+
+    function it_throws_an_exception_if_attribute_does_not_exist(
+        $structureConfigLoader,
+        AssetFamilyCode $familyCode,
+        AssetAttributeCode $attributeCode
+    ): void {
+        $structureConfigLoader->load()->willReturn($this->getConfig());
+
+        $familyCode->__toString()->willReturn('packshot');
+        $attributeCode->__toString()->willReturn('description');
+
+        $this
+            ->shouldThrow(
+                new \RuntimeException()
+            )
+            ->during('build', [$familyCode, $attributeCode]);
+    }
+
+    private function getConfig(): array
+    {
+        return [
+            'packshot' => [
+                'attributes' => [
+                    [
+                        'code' => 'locale',
+                        'type' => 'text'
+                    ],
+                ],
+            ],
+        ];
+    }
+}
