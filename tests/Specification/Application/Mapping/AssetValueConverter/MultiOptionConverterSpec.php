08bef4c4dc456daaebf4370bf326e9b7abe44f52
Willy <willy.mesnage@akeneo.com>
2019-09-26 11:23:32 +0200

    API-693: Fix multi option converter spec

--- tests/Specification/Application/Mapping/AssetValueConverter/MultiOptionConverterSpec.php
+++ tests/Specification/Application/Mapping/AssetValueConverter/MultiOptionConverterSpec.php
@@ -5,15 +5,13 @@ namespace Specification\AkeneoDAMConnector\Application\Mapping\AssetValueConvert
 
 use AkeneoDAMConnector\Application\Mapping\AssetValueConverter;
 use AkeneoDAMConnector\Application\Mapping\AssetValueConverter\MultiOptionConverter;
-use AkeneoDAMConnector\Domain\Asset\DamAsset;
 use AkeneoDAMConnector\Domain\Asset\DamAssetValue;
-use AkeneoDAMConnector\Domain\AssetAttribute;
-use AkeneoDAMConnector\Domain\Locale;
+use AkeneoDAMConnector\Tests\Specification\Builder\AssetAttributeBuilder;
+use AkeneoDAMConnector\Tests\Specification\Builder\DamAssetBuilder;
 use PhpSpec\ObjectBehavior;
 
 class MultiOptionConverterSpec extends ObjectBehavior
 {
-
     public function it_is_a_multi_option_converter(): void
     {
         $this->shouldHaveType(MultiOptionConverter::class);
@@ -25,35 +23,28 @@ class MultiOptionConverterSpec extends ObjectBehavior
         $this->getSupportedType()->shouldReturn('multiple_options');
     }
 
-    public function it_converts_a_dam_asset_value_into_a_localized_pim_asset_value(
-        DamAsset $damAsset,
-        DamAssetValue $damAssetValue,
-        AssetAttribute $attribute,
-        Locale $locale
-    ): void {
-        $damAssetValue->value()->willReturn('blue, yellow, brown');
-        $attribute->isLocalizable()->willReturn(true);
-        $damAsset->locale()->willReturn($locale);
-        $locale->__toString()->willReturn('en_US');
+    public function it_converts_a_dam_asset_value_into_a_localized_pim_asset_value(): void
+    {
+        $damAsset = DamAssetBuilder::build('table', 'packshot', ['colors' => 'simic, blue, green'], 'en_US');
+        $attribute = AssetAttributeBuilder::build('colors', 'multiple_options', true);
+        $damAssetValue = new DamAssetValue('colors', 'simic, blue, green');
 
         $pimValue = $this->convert($damAsset, $damAssetValue, $attribute);
         $pimValue->getAttribute()->shouldReturn($attribute);
-        $pimValue->getData()->shouldReturn(['blue', 'yellow', 'brown']);
+        $pimValue->getData()->shouldReturn(['simic', 'blue', 'green']);
         $pimValue->getLocale()->shouldReturn('en_US');
         $pimValue->getChannel()->shouldReturn(null);
     }
 
-    public function it_converts_a_dam_asset_value_into_a_not_localized_pim_asset_value(
-        DamAsset $damAsset,
-        DamAssetValue $damAssetValue,
-        AssetAttribute $attribute
-    ): void {
-        $damAssetValue->value()->willReturn('blue, yellow, brown');
-        $attribute->isLocalizable()->willReturn(false);
+    public function it_converts_a_dam_asset_value_into_a_not_localized_pim_asset_value(): void
+    {
+        $damAsset = DamAssetBuilder::build('table', 'packshot', ['colors' => 'simic, blue, green']);
+        $attribute = AssetAttributeBuilder::build('colors', 'multiple_options', false);
+        $damAssetValue = new DamAssetValue('colors', 'simic, blue, green');
 
         $pimValue = $this->convert($damAsset, $damAssetValue, $attribute);
         $pimValue->getAttribute()->shouldReturn($attribute);
-        $pimValue->getData()->shouldReturn(['blue', 'yellow', 'brown']);
+        $pimValue->getData()->shouldReturn(['simic', 'blue', 'green']);
         $pimValue->getLocale()->shouldReturn(null);
         $pimValue->getChannel()->shouldReturn(null);
     }
