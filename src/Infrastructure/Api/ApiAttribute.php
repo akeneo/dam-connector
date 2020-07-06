31458d76e236f6ab6a6a7ab3cd5941cfe79046d7
Thomas Galvaing <thomas.galvaing@akeneo.com>
2020-07-06 17:37:59 +0200

    solution 1 product

---
+++ src/Infrastructure/Api/ApiAttribute.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PimApiTest\Infrastructure\Api;
+
+use Akeneo\PimEnterprise\ApiClient\AkeneoPimEnterpriseClientInterface;
+use PimApiTest\Infrastructure\Pim\ClientBuilder;
+
+class ApiAttribute
+{
+    /** @var AkeneoPimEnterpriseClientInterface */
+    private $client;
+
+    public function __construct(ClientBuilder $clientBuilder)
+    {
+        $this->client = $clientBuilder->getClient();
+    }
+
+    public function get(array $product): array
+    {
+        return $product["values"];
+    }
+}
